71a84d4256e770cbecec1c38e50d7f5f35caf4b7
An0n-xen <fyfybaisieamoah6@gmail.com>
2026-03-22 12:22:05 +0000

    auto create repo on vps

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Settings(BaseSettings):
     MAX_CONCURRENT_API_CALLS: int = 4
     SERVICE_CACHE_TTL_SECONDS: int = 300
     OAUTH_CALLBACK_PORT: int = 8090
-    METRICS_PORT: int = 9090
+    METRICS_PORT: int = 9091
     STATE_DIR: str = "data/state"
 
     GOOGLE_SCOPES: list[str] = [
